edbd7ae436926eb156eb368884a3faddb4865710
Jasand <smileonyourface@yandex.com>
2023-12-18 00:07:57 +0500

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 2: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int columns = new Random().Next(1, 11);
+int lines = new Random().Next(1, 11);
+int[,] matrix = new int[lines, columns];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив: ");
+PrintMatrix(matrix);
+
+int[] temp = new int[matrix.GetLength(1)];
+for (int i = 0; i < matrix.GetLength(1); i++)
+{
+    temp[i] = matrix[0, i];
+    matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+    matrix[matrix.GetLength(0) - 1, i] = temp[i];
+}
+Console.WriteLine("Конечный массив: ");
+PrintMatrix(matrix);
